0e1bbb9f7fc2992df5ed693476b60e1214c1a89e
Denis Leung <denis@Deniss-MacBook-Pro.local>
2024-06-23 17:46:39 +1000

    Added preloader page

--- scripts.js
+++ scripts.js
@@ -20,8 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
     copyEmailButton.addEventListener('click', () => {
         navigator.clipboard.writeText("sr@srxlct.com").then(() => {
             showCustomPopup("Email copied to clipboard!");
+            copyEmailButton.classList.add('copied');
             copyEmailButton.innerHTML = '<i class="fas fa-check"></i> Copied';
             setTimeout(() => {
+                copyEmailButton.classList.remove('copied');
                 copyEmailButton.innerHTML = '<i class="fas fa-copy"></i> Copy email';
             }, 3000);
         });
@@ -44,4 +46,21 @@ document.addEventListener("DOMContentLoaded", () => {
             }, 500);
         }, 3000);
     }
+
+    // GSAP animation for SVG draw
+    gsap.to(".draw", {
+        duration: 2,
+        strokeDashoffset: 0,
+        ease: "power2.inOut",
+        onComplete: () => {
+            const preloader = document.getElementById('preloader');
+            gsap.to(preloader, {
+                duration: 0.5,
+                opacity: 0,
+                onComplete: () => {
+                    preloader.style.display = 'none';
+                }
+            });
+        }
+    });
 });
